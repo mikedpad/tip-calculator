80d7cf74090beba2fec36f5cdbef4203dbde75ec
Michael Daniel Padilla <michaeldpadilla@gmail.com>
2021-02-20 03:35:23 -0800

    Added custom styles for Header to stylesheet

--- src/components/UI/Header.jsx
+++ src/components/UI/Header.jsx
@@ -1,32 +1,13 @@
 import PropTypes from 'prop-types';
-import { Col, Row, Typography } from 'antd';
-import AppSettingsButton from './Buttons/AppSettingsButton';
+import { Typography } from 'antd';
 
 const { Title } = Typography;
 
 const Header = ({ title }) => (
-  <header
-    style={{
-      backgroundColor: `#e6e6ef`,
-      boxShadow: `0 2px 8px rgba(0, 0, 0, 0.2)`,
-      minHeight: 60,
-      margin: `0 0 16px`,
-      padding: `0 16px`,
-      position: `fixed`,
-      left: 0,
-      right: 0,
-      top: 0,
-      zIndex: 3,
-    }}
-  >
-    <Row align="middle" wrap={false}>
-      <Col flex="auto">
-        <Title style={{ margin: 0, fontSize: 32 }}>{title}</Title>
-      </Col>
-      <Col flex="0 1">
-        <AppSettingsButton />
-      </Col>
-    </Row>
+  <header className="app-header">
+    <Title className="app-title" style={{ margin: 0, fontSize: 32 }}>
+      {title}
+    </Title>
   </header>
 );
 
